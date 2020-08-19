6daa85634c294dee806b0cee85f58fa78aec3b2f
tbonza <tylers.pile@gmail.com>
2020-08-19 15:03:23 -0400

    adding distributed pmap implementation, setting up for unit tests

--- src/Umbrella.jl
+++ src/Umbrella.jl
@@ -1,5 +1,7 @@
 module Umbrella
 
+using Distributed
+
 export example1, regexes, text
 
 function example1(text::Array{String,1}, regexes::Array{Regex,1})::Bool
@@ -10,6 +12,18 @@ function example1(text::Array{String,1}, regexes::Array{Regex,1})::Bool
         end
     end
     return true
+end
+
+function example2(text::String, regexes::Array{Regex,1})::Bool
+    for r in regexes
+        if occursin(r,text)
+        end
+    end
+    return true
+end
+
+function dist_example2(text::Array{String,1}, regexes::Array{Regex,1}, batch::Int)::Array{Bool,1}
+    return pmap(x -> example2(x, regexes), text, batch_size=batch, on_error=ex->false)
 end
 
 end # module
